8891f8b68edc7338904c7a7f0e8fc75323a0cf80
Masputrawae <masputrawae.official@gmail.com>
2025-05-24 23:20:15 +0700

    Changed On 📅 : 2025-05-24 23:20:15

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,46 @@
-const tagline = JSON.parse(document.getElementById("tagline").dataset.taglines)
-console.log(tagline)
+// ===== SIDEBAR ===== //
+function sidebarAction() {
+  const toggleSidebar = document.querySelectorAll('[data-sidebar]')
+  toggleSidebar.forEach(e => {
+    const targetId = e.getAttribute('data-sidebar')
+    console.log(e)
+    e.addEventListener('click', () => {
+      document.getElementById(targetId).classList.toggle('sidebar--active')
+    })
+  })
+}
+
+sidebarAction()
+
+// ===== SWITCH THEME ===== //
+function setTheme() {
+  const STORAGE_THEME_KEY = 'storage-theme'
+
+  const btnTheme = document.getElementById('switchTheme')
+  const elHTML = document.documentElement
+
+  let storageTheme = localStorage.getItem(STORAGE_THEME_KEY)
+  let defaultTheme = elHTML.getAttribute('data-theme')
+  
+  if(elHTML.getAttribute('data-theme') === null){
+    defaultTheme = "dark"
+  }
+
+  if (storageTheme === null) {
+    localStorage.setItem(STORAGE_THEME_KEY, defaultTheme)
+  }
+
+  let currentTheme = (storageTheme === null) ? defaultTheme : storageTheme
+
+  elHTML.setAttribute('data-theme', currentTheme)
+
+  btnTheme.addEventListener("click", ()=>{
+    let initTheme = elHTML.getAttribute('data-theme')
+    let changeTheme = (initTheme === "dark") ? "light" : (initTheme === "light" ? "dark" : initTheme)
+
+    elHTML.setAttribute('data-theme', changeTheme)
+    localStorage.setItem(STORAGE_THEME_KEY, changeTheme)
+  })
+}
+
+setTheme()
---
+++ content/_index.md
@@ -0,0 +1,15 @@
+---
+title: Home
+date: 2025-05-13T05:19:23+07:00
+update: 2025-05-13T05:19:23+07:00
+tags:
+  - notes
+---
+
+# 👋 Selamat Datang!
+
+Di sini adalah tempatku menulis dan menyimpan hal-hal yang kupelajari — _mungkin?_  
+Isinya acak, tak menentu, seperti menunggu hujan tapi cuma mendung 🤓  
+Mungkin lebih tepatnya: tempat mencurahkan isi hati 😁
+
+**Silakan ambil sesuatu yang sekiranya berguna bagimu** _kalau ada_ 😁
