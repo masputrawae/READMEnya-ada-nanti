3f359f4ba1681e2d6264e97439d42de5eb00d246
Masputrawae <masputrawae.official@gmail.com>
2025-06-11 16:37:15 +0700

    Changed On 📅 : 2025-06-11 16:37:15

--- src/data/site.js
+++ src/data/site.js
@@ -11,10 +11,15 @@ export const SOCIAL = [
   { name: 'Discord', url: 'https://example.com', icon: 'icon-discord' },
 ]
 
-export const MENU = [
+export const MENU_NAVIGATION = [
   { name: 'Home', url: '/', icon: 'icon-home' },
   { name: 'About', url: '/about', icon: 'icon-person-circle' },
   { name: 'Projects', url: '/projects', icon: 'icon-wrench-screwdriver' },
   { name: 'Blog', url: '/blog', icon: 'icon-pencil-square' },
   { name: 'Contect', url: '/contact', icon: 'icon-contact' },
+]
+
+export const CTA = [
+  {name: "About", url: "/about", primary: true},
+  {name: "Discord", url: "https://example.com", primary: false},
 ]
