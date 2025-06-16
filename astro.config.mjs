05e112f1822575e17eaa22ab4288439273b7fa60
Masputrawae <masputrawae.official@gmail.com>
2025-06-16 18:48:50 +0700

    Changes to be committed: 	modified:   astro.config.mjs 	modified:   src/components/Cards.astro 	modified:   src/components/FeaturedProjects.astro 	modified:   src/components/Footer.astro 	modified:   src/components/Sidebar.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
+	image: {
 		domains: ['astro.build'],
 		remotePatterns: [{ protocol: 'https' }],
 		service: passthroughImageService()
