737ac296224a4452cf3872255cdb1d3347361351
Masputrawae <masputrawae.official@gmail.com>
2025-07-07 09:49:34 +0700

    Changed On 📅 : 2025-07-07 09:49:34

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://masputrawae.github.io',
-  base: '/',
-  integrations: [icon()],
+	site: 'https://masputrawae.github.io',
+	base: '/',
+	devToolbar: { enabled: false },
+	integrations: [icon()],
 
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+	vite: {
+		plugins: [tailwindcss()]
+	}
+})
